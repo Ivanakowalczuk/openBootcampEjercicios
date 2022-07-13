cbd5cfe8a8dac6ca4471a1fc03df6f2961e16962
ivanakowalczuk <ivanakowalczuk@gmail.com>
2022-07-13 17:26:14 -0300

    tercer commit

---
+++ clase15Ejercicio/index.js
@@ -0,0 +1,13 @@
+const nombre = "Ivana"
+const apellido = "Kowalczuk"
+
+const persona = {
+    nombre,
+    apellido,
+}
+
+sessionStorage.setItem("persona", JSON.stringify(persona))
+localStorage.setItem("persona", JSON.stringify(persona))
+
+const now = new Date()
+document.cookie = `persona=${JSON.stringify(persona)};expires=${new Date(now.getTime() + 2 * 60000)}`
